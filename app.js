c92d15591d91b7a25b88ac93146ee56609b2d2c4
Splinter1080 <supreetjoshi2k15@gmail.com>
2022-05-18 20:42:00 +0530

    added Procfile

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(bodyParser.json())
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const cors = require("cors");
 
 //----------------------
