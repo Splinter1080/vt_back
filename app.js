896113b5f93ecec2b10cf80052872223236eb713
Splinter1080 <supreetjoshi2k15@gmail.com>
2022-05-18 22:08:27 +0530

    test deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(bodyParser.json())
-const PORT = process.env.PORT || 5000
+
 const cors = require("cors");
 
 //----------------------
@@ -397,7 +397,8 @@ app.get("/assets", async (req, res) => {
 app.get('/', (req, res) => {
     res.send("hello world !");
 })
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 });
 
